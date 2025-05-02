a2712b2145c3eb64dc3485d48a72ddc8309aa7d7
Marcelo Junqueira <marcelojunqueiraf@gmail.com>
2025-05-01 22:00:47 -0300

    corrige template de router

--- templates/routerTemplate.py
+++ templates/routerTemplate.py
@@ -3,22 +3,22 @@ from app.services.NOME_RECURSO import create, getOne, getAll, update, delete
 
 router = APIRouter()
 
-@router.post()
-async def create(payload):
+@router.post("", status_code=201)
+async def create_endpoint(payload):
     return create(payload)
 
-@router.get("/{id}")
-async def getOne(id):
+@router.get("/{id}", status_code=200)
+async def getOne_endpoint(id):
     return getOne(id)
 
-@router.get("/")
-async def getAll():
+@router.get("/", status_code=200)
+async def getAll_endpoint():
     return getAll()
 
-@router.put("/{id}")
-async def update(id, payload):
+@router.put("/{id}", status_code=200)
+async def update_endpoint(id, payload):
     return update(id, payload)
 
-@router.delete("/{id}")
-async def delete(id):
+@router.delete("/{id}", status_code=204)
+async def delete_endpoint(id):
     return delete(id)
